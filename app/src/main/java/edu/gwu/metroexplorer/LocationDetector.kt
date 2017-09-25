2fa3b66a2a86a3f7a2273d57599fd429aed9c910
cmahajan1891 <chetan_mahajan@gwu.edu>
2017-09-25 18:33:57 -0400

    Partial Current Location Changes

--- app/src/main/java/edu/gwu/metroexplorer/LocationDetector.kt
+++ app/src/main/java/edu/gwu/metroexplorer/LocationDetector.kt
@@ -1,45 +1,11 @@
 package edu.gwu.metroexplorer
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.support.v4.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-
 
 /**
  * Created by cmahajan on 9/23/17.
  */
 
+
 class LocationDetector {
 
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
-    lateinit var latLng: LatLng
-
-    private fun getProvider(activity: MapsActivity): FusedLocationProviderClient {
-
-        return LocationServices.getFusedLocationProviderClient(activity)
-    }
-
-    fun getLastKnownLocation(activity: MapsActivity, mMap: GoogleMap) {
-        mFusedLocationClient = getProvider(activity)
-        if (ContextCompat.checkSelfPermission(activity,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mFusedLocationClient.lastLocation
-                    .addOnSuccessListener(activity, { location ->
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            latLng = LatLng(location.latitude, location.longitude)
-                            mMap.addMarker(MarkerOptions().position(latLng).title("Marker at your location."))
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-                        }
-                    })
-        }
-
-    }
 }
