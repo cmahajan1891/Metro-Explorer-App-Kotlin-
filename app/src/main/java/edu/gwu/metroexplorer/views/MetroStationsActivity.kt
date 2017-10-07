f019e31bbf6e372ffb7809810c6792a3d0d7adb0
Edwin Abraham <edwin.abraham12@gmail.com>
2017-10-06 20:57:23 -0400

    All  remaining activity view added

---
+++ app/src/main/java/edu/gwu/metroexplorer/views/MetroStationsActivity.kt
@@ -0,0 +1,13 @@
+package edu.gwu.metroexplorer.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import edu.gwu.metroexplorer.R
+
+class MetroStationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metro_stations)
+    }
+}
