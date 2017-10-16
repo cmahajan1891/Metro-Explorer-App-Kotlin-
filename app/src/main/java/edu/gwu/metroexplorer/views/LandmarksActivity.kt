34023b221cc655b38d44cb25d8ca872df413693e
Chetan Mahajan <chetan_mahajan@gwu.edu>
2017-10-16 01:57:21 -0400

    Clickable Implemented in Landmarks Activity

--- app/src/main/java/edu/gwu/metroexplorer/views/LandmarksActivity.kt
+++ app/src/main/java/edu/gwu/metroexplorer/views/LandmarksActivity.kt
@@ -1,15 +1,20 @@
 package edu.gwu.metroexplorer.views
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import edu.gwu.metroexplorer.R
 import kotlinx.android.synthetic.main.activity_landmarks.*
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import edu.gwu.metroexplorer.model.StationData
 
 
 class LandmarksActivity : AppCompatActivity() {
@@ -22,17 +27,31 @@ class LandmarksActivity : AppCompatActivity() {
         landmarksRecyclerView.layoutManager = LinearLayoutManager(this);
 
         val samples = arrayOf("January", "February", "March")
-        var landmarksAdapter = LandmarksAdapter(samples)
+        var landmarksAdapter = LandmarksAdapter(samples, this@LandmarksActivity)
         landmarksRecyclerView.adapter = landmarksAdapter
     }
 }
 
-class LandmarksAdapter(private val dataSet: Array<String>) : RecyclerView.Adapter<LandmarksAdapter.ViewHolder>() {
+class LandmarksAdapter(private val dataSet: Array<String>, private val landmarksActivity: LandmarksActivity) : RecyclerView.Adapter<LandmarksAdapter.ViewHolder>() {
 
     //Array<YelpLandmark>
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageView)
+        val gradientView: ImageView = view.findViewById(R.id.gradientView)
+        val textView4: TextView = view.findViewById(R.id.textView4)
+        val ratingBar: RatingBar = view.findViewById(R.id.ratingBar)
+        val isOpenTextView: TextView = view.findViewById(R.id.isOpenTextView)
+        val distance: TextView = view.findViewById(R.id.distance)
+    }
+
+    private fun callIntent(){
+        Log.d("Clickable", "called")
+        val intent = Intent(landmarksActivity, LandmarkDetailActivity::class.java)
+
+        val samples = arrayOf("January", "February", "March")
+        intent.putExtra("yelpDataSet", samples)
+        landmarksActivity.startActivity(intent)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
